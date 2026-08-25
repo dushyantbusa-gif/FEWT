7226b38fd9d6752bf571b0ecafee0fbd629f5f3f
Dushyant Busa <dusyantbusa@gmail.com>
2026-08-25 15:34:02 +0530

    Add files via upload

---
+++ lab-20/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello, Dushyant!</h1>;
+  }
+}
+
+// let fun = a() => (class App extends React.Component{
+//    render() {
+//     return <h1>Hello, Dushyant!</h1>;
+//   }
+// });
+export default App;
